0835b8bc86bd485b07fffcfd047548cd3a8ef4ec
Diego Araujo <diegooaraujoo2307@gmail.com>
2026-08-12 13:33:42 -0300

    fix: configure relative base path and HashRouter for GitHub Pages support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -59,7 +59,7 @@ const App = () => (
       <ScrollTracker />
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <AuthProvider>
           <div className="relative min-h-screen w-full overflow-x-hidden">
             {/* Background Canvas */}
@@ -108,7 +108,7 @@ const App = () => (
             </div>
           </div>
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
